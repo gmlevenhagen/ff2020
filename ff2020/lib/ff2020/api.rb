474fc4245a43149dfc991eb6ec32d7a0c1ab8741
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-19 20:39:38 +0000

    updated api

--- ff2020/lib/ff2020/api.rb
+++ ff2020/lib/ff2020/api.rb
@@ -1,5 +1,5 @@
 class Ff2020::Api
-  
+  attr_accessor :name, :team, :url
   
   #https://www.fantasypros.com/nfl/rankings/qb-cheatsheets.php
   #<link rel="canonical" href="https://www.fantasypros.com/nfl/rankings/qb-cheatsheets.php">
@@ -23,8 +23,8 @@ class Ff2020::Api
 
     ranks = self.new
     ranks.name = doc.search("h1.profile-name").text.strip
-    ranks.team = doc.search("href="/kansas-city-chiefs"").text
-    ranks.url = "<script type="text/javascript" src="https://api.getdrip.com/client/track?url=https%3A%2F%2Ffantasyfootballcalculator.com%2Fplayers%2Fpat-mahomes&amp;visitor_uuid=5141e363ca1647fca6eeef6693d12c19&amp;category=Players&amp;label=Pat%20Mahomes&amp;_action=Player%20Page%20Viewed&amp;drip_account_id=3742580&amp;callback=Drip_182813046"></script>"
+    ranks.team = doc.search("href.kansas-city-chiefs").text
+    ranks.url = "https://fantasyfootballcalculator.com/players/pat-mahomes"
 
 
     ranks
@@ -34,13 +34,20 @@ class Ff2020::Api
     doc = Nokogiri::HTML(open("https://cbssports.com"))
 
     ranks = self.new
-    ranks.name = doc.search("h1.class = "player-name"").text
+    ranks.name = doc.search("h1.player-name").text.strip
     ranks.team = doc.search("href="/nfl/teams/BAL/baltimore-ravens/"").text
     ranks.url = "https://www.cbssports.com/fantasy/football/players/2181169/lamar-jackson/"
 
     ranks
   end
 end
+  
+
+
+  
+  
+  
+  
   
   
  
