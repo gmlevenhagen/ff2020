990a4bfcbfe39ec5ed2d2826b7a0e0df1290acd6
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-30 12:30:53 +0000

    updated with comments

--- ff2020/lib/ff2020/scraper.rb
+++ ff2020/lib/ff2020/scraper.rb
@@ -1,9 +1,11 @@
 class Ff2020::Scraper
   attr_accessor :name, :url
 
+#Scraper is meant to pull the names from rankings list from websites and generate a list through CLI
+
 
 def self.scrape_fp
-    main_url = "http://fantasypros.com"
+    url = "http://fantasypros.com"
     doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/rankings/qb-cheatsheets.php"))
 
     i = 0
@@ -15,24 +17,26 @@ def self.scrape_fp
   end
 
   def self.scrape_nfl
-    main_url = "http://nfl.com"
+    url = "http://nfl.com"
     doc = Nokogiri::HTML(open("https://fantasy.nfl.com/research/projections"))
 
     i = 0
     while i < 6
       qb_two = self.new
+      binding.pry
       qb_two.name = doc.search("td")[i].text
       i += 1
     end
   end
   
   def self.scrape_cbs
-    main_url = "http://cbssports.com"
+    url = "http://cbssports.com"
     doc = Nokogiri::HTML(open("https://www.cbssports.com/fantasy/football/players/2142052/patrick-mahomes/"))
 
     i = 0
     while i < 6
       qb_three = self.new
+      binding.pry
       qb_three.name = doc.search("h1").text
       i += 1
     end
